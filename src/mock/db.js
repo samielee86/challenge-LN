e10c6fca031f212f0e5f8908cdb2f9063592aca8
Samie Lee <samielee86@gmail.com>
2022-10-27 19:42:08 -0300

    Configuracion de sass y armado de dummy de 3 arts

---
+++ src/mock/db.js
@@ -0,0 +1,40 @@
+const db = [
+    {
+        img: 'https://res.cloudinary.com/dhwlnxbib/image/upload/v1666892698/Challenge%20LN/UO4LJGTNDVGYVNPW4KRZD7HG7U_ibi26k.webp',
+        lead: 'Trabas y diferencias.',
+        head: 'El Gobierno intenta “reflotar” el acuerdo comercial entre el Mercosur y la Unión Europea',
+        subhead: 'En el marco de la reunión de cancilleres de la Celac y Europa, negocian para reactivar el vínculo, firmado en 2019; Cafiero habló de “asimetrías” y Borrell de la “preocupación por la deforestación” en Brasil.',
+        authorMarquee: 'Jaime Rosemberg',
+        authorImg: 'https://res.cloudinary.com/dhwlnxbib/image/upload/v1666892704/Challenge%20LN/2757774_qbxwou.webp',
+        url: 'https://www.lanacion.com.ar/politica/el-gobierno-intenta-reflotar-el-acuerdo-comercial-entre-el-mercosur-y-la-union-europea-nid27102022/'
+    },
+    {
+        img: 'https://res.cloudinary.com/dhwlnxbib/image/upload/v1666894114/Challenge%20LN/NZQ2JKCFERA6NM4ZSX3DI735EI_odeqpy.webp',
+        lead: 'Inflación'
+        head: 'Hugo Moyano firmó un aumento de 107% para Camioneros y son cada vez más los gremios que negocian tres dígitos',
+        subhead: 'De esta manera, desactivó el paro sectorial al que había convocado para el lunes próximo; además, acordó un bono de fin de año de $100.000',
+        authorMarquee: 'Nicolás Balinotti',
+        authorImg: 'https://res.cloudinary.com/dhwlnxbib/image/upload/v1666894117/Challenge%20LN/2e12cfa4-5e1d-44ea-8f0c-33fe1da1e650_dmlp0l.webp',
+        url: 'https://www.lanacion.com.ar/politica/hugo-moyano-logro-un-aumento-de-107-para-camioneros-y-son-cada-vez-mas-los-gremios-que-negocian-tres-nid27102022/'
+    },
+    {
+        img: 'https://res.cloudinary.com/dhwlnxbib/image/upload/v1666894594/Challenge%20LN/YJ5RVECDOJCMHLTQC4KQDBIVAM_wso0ve.webp',
+        lead: 'Elecciones 2023',
+        head: 'Eduardo “Wado” De Pedro admitió que intentan convencer a Alberto Fernández para que no haya PASO el año próximo',
+        subhead: 'El ministro del Interior dijo que los gobernadores y los intendentes pretenden que se diriman las candidaturas puertas adentro del Frente de Todos; también respondió a las versiones que lo apuntan como un presidenciable',
+        authorMarquee: 'LA NACION',
+        authorImg: null,
+        url: 'https://www.lanacion.com.ar/politica/eduardo-wado-de-pedro-admitio-que-intentan-convencer-a-alberto-fernandez-para-que-no-haya-paso-el-nid27102022/'
+    }
+    // {
+    //     img:
+    //     lead:
+    //     head:
+    //     subhead:
+    //     authorMarquee:
+    //     authorImg:
+    //     url:
+    // }
+]
+
+export default db;
