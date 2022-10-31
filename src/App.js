bac7d53fb8da8606b80c4d893b8715425ff1b7e9
Samie Lee <samielee86@gmail.com>
2022-10-31 20:42:34 -0300

    creacion de card y estilos basicos + mock

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const App = () => {
-	return <div>Challenge LN</div>;
-};
-
-export default App;
---
+++ src/components/Containers/FocalIzquierdo.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Card from "../Card/Card";
+
+const FocalIzquierdo = () => {
+	return <Card />;
+};
+
+export default FocalIzquierdo;
